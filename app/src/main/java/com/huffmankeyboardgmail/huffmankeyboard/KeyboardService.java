5e5111055bab5cfd7315bd23862012d0a35bdf91
GrantMoyer <grantmoyer@gmail.com>
2015-08-03 20:42:54 -0400

    Initial commit

---
+++ app/src/main/java/com/huffmankeyboardgmail/huffmankeyboard/KeyboardService.java
@@ -0,0 +1,11 @@
+package com.huffmankeyboardgmail.huffmankeyboard;
+
+import android.content.Intent;
+import android.inputmethodservice.InputMethodService;
+import android.os.IBinder;
+
+public class KeyboardService extends InputMethodService {
+    public KeyboardService() {}
+
+
+}
